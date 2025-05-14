30f8f553e2656263d812c297f52411abb5bfc052
Dilan1348 <dilanhm64@gmail.com>
2025-05-14 10:21:23 +0800

    adding workbox

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,3 +1,59 @@
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+// import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { NetworkFirst, CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { BASE_URL } from './config';
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+    ({ url }) => {
+        return url.origin === 'https://fonts.googleapis.com' ||
+            url.origin === 'https://fonts.gstatic.com';
+    },
+    new CacheFirst({
+        cacheName: 'google-fonts',
+    })
+);
+
+registerRoute(
+    ({ url }) => {
+        return url.origin === 'https://cdnjs.cloudflare.com' || url.origin.includes('fontawesome');
+    },
+    new CacheFirst({
+        cacheName: 'fontawesome',
+    }),
+);
+
+registerRoute(
+    ({ request, url }) => {
+        const baseUrl = new URL(BASE_URL);
+        return baseUrl.origin === url.origin && request.destination !== 'image';
+    },
+    new NetworkFirst({
+        cacheName: 'story-api',
+    }),
+);
+
+registerRoute(
+    ({ request, url }) => {
+        const baseUrl = new URL(BASE_URL);
+        return baseUrl.origin === url.origin && request.destination === 'image';
+    },
+    new StaleWhileRevalidate({
+        cacheName: 'story-api-images',
+    }),
+);
+
+registerRoute(
+    ({ url }) => {
+        return url.origin.includes('maptiler');
+    },
+    new CacheFirst({
+        cacheName: 'maptiler-api',
+    }),
+);
+
 self.addEventListener('push', (event) => {
     console.log('Service worker pushing...');
 
